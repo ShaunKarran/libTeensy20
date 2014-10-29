a5d4baf7e1aa9010061336f6dc19ff9fe7957d90
Shaun Karran <shaun.karran@gmail.com>
2014-10-29 14:12:36 +1000

    Added basic functionality to adc.c.

--- include/adc.h
+++ include/adc.h
@@ -4,9 +4,29 @@
  *	Created:		October 2014
  */
 
- #ifndef _adc_h
- #define _adc_h
+#include <stdint.h>
 
+#ifndef _adc_h
+#define _adc_h
 
+// Prescaler values (based on 16Mhz clkSpd)
+#define ADC_8MHZ 1
+#define ADC_4MHZ 2
+#define ADC_2MHZ 3
+#define ADC_1MHZ 4
+#define ADC_500KHZ 5
+#define ADC_250KHZ 6
+#define ADC_125KHZ 7
 
- #endif // _adc_h
+#define ADC_MODE_8_BIT 0
+#define ADC_MODE_10_BIT 1
+
+void adc_init(unsigned char prescaler);
+
+uint16_t adc_read(unsigned char channel, unsigned char mode);
+
+uint32_t adc_read_scaled(unsigned char channel, unsigned char mode, uint32_t scale);
+
+void adc_freerun(unsigned char channel, unsigned char enable);
+
+#endif // _adc_h
