6531470822a9e47150b10a77778c15af0e50102c
Shaun Karran <shaun.karran@gmail.com>
2014-10-29 14:47:22 +1000

    Added basic timer functionality.

--- include/timer.h
+++ include/timer.h
@@ -4,9 +4,23 @@
  *	Created:		October 2014
  */
 
- #ifndef _timer_h
- #define _timer_h
+#ifndef _timer_h
+#define _timer_h
 
+#define TIMER_PRESCALE_1	1
+#define TIMER_PRESCALE_8	2
+#define TIMER_PRESCALE_64	3
+#define TIMER_PRESCALE_256	4
+#define TIMER_PRESCALE_1024	5
 
+#define TIMER_OCR_A 0
+#define TIMER_OCR_B 1
+#define TIMER_OCR_C 2
 
- #endif // _timer_h
+void timer1_ovf_init(unsigned char prescaler);
+
+void timer1_freq_Hz(unsigned char hz);
+
+void timer1_freq_ms(unsigned char ms);
+
+#endif // _timer_h
--- src/timer.c
+++ src/timer.c
@@ -4,3 +4,45 @@
  *	Created:		October 2014
  */
 
+#include <stdint.h>
+
+#include "../include/timer.h"
+
+void timer1_ovf(unsigned char prescaler) {
+	TCCR1B |= prescaler;
+    TCNT1 = 0; // Start timer at 0.
+    SET_BIT(TIMSK1, TOIE1); // Enable overflow interrupt.
+}
+
+void timer1_freq_Hz(unsigned char hz, unsigned char ocr) {
+	uint32_t ocrValue;
+    int prescale[] = { 1, 8, 64, 256, 1024};
+    int i = 0;
+
+    do {
+        // ocr = (clk / (prescale * desiredFreq)) - 1
+        ocrValue = (16000000 / (prescale[i] * interruptFreq)) - 1;
+        i++;
+    } while (ocrValue > 65535); // While ocrValue is too large, try larger prescaler.
+
+    TCNT1 = 0; // Set counter to 0.
+    TCCR1B |= i; // Set prescaler.
+    set_bit(TCCR1B, WGM12); // Set CTC mode.
+
+    if (ocr == TIMER_OCR_A) {
+    	OCR1A = ocrValue; // Set compare A value.
+    	set_bit(TIMSK1, OCIE1A); // Enable compare A interrupt.
+
+    } else if (ocr == TIMER_OCR_B) {
+    	OCR1B = ocrValue; // Set compare A value.
+    	set_bit(TIMSK1, OCIE1B); // Enable compare A interrupt.
+
+    } else if (ocr == TIMER_OCR_C) {
+    	OCR1C = ocrValue; // Set compare A value.
+    	set_bit(TIMSK1, OCIE1C); // Enable compare A interrupt.
+    }
+}
+
+void timer1_freq_ms(unsigned char ms) {
+	// TODO
+}
