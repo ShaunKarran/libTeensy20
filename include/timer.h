4a970d822d0f946f13a4d8f1c531d79869411554
Shaun Karran <shaun.karran@gmail.com>
2014-10-30 18:08:16 +1000

    Function headers. timer1_freq_ms functionality added.

--- include/timer.h
+++ include/timer.h
@@ -7,20 +7,23 @@
 #ifndef _timer_h
 #define _timer_h
 
+// Defines --------------------------------------------------------------------
 #define TIMER_PRESCALE_1	1
 #define TIMER_PRESCALE_8	2
 #define TIMER_PRESCALE_64	3
 #define TIMER_PRESCALE_256	4
 #define TIMER_PRESCALE_1024	5
 
+// Output compare registers
 #define TIMER_OCR_A 0
 #define TIMER_OCR_B 1
 #define TIMER_OCR_C 2
 
+// Function Prototypes --------------------------------------------------------
 void timer1_ovf_init(unsigned char prescaler);
 
-void timer1_freq_Hz(unsigned char hz);
+void timer1_freq_Hz(uint32_t hz, unsigned char ocr);
 
-void timer1_freq_ms(unsigned char ms);
+void timer1_freq_ms(uint16_t ms, unsigned char ocr);
 
 #endif // _timer_h
--- src/timer.c
+++ src/timer.c
@@ -4,28 +4,47 @@
  *	Created:		October 2014
  */
 
+// Includes -------------------------------------------------------------------
 #include <stdint.h>
 
 #include "../include/timer.h"
 
+/*
+ * Enable timer1 with overflow interrupt
+ *
+ * @param unsigned char prescaler
+ *      Using prescaler defines in timer.h
+ *
+ * @return void
+*/
 void timer1_ovf(unsigned char prescaler) {
 	TCCR1B |= prescaler;
     TCNT1 = 0; // Start timer at 0.
     SET_BIT(TIMSK1, TOIE1); // Enable overflow interrupt.
 }
 
-void timer1_freq_Hz(unsigned char hz, unsigned char ocr) {
+/*
+ * Enable timer1 at desired frequency.
+ *
+ * @param unsigned char hz
+ *      Frequency of interrupt in Hz
+ *
+ * @param unsigned char ocr
+ *      Output compare register to use
+ *
+ * @return void
+*/
+void timer1_freq_Hz(uint32_t hz, unsigned char ocr) {
 	uint32_t ocrValue;
     int prescale[] = { 1, 8, 64, 256, 1024};
     int i = 0;
 
     do {
         // ocr = (clk / (prescale * desiredFreq)) - 1
-        ocrValue = (16000000 / (prescale[i] * interruptFreq)) - 1;
+        ocrValue = (16000000 / (prescale[i] * hz)) - 1;
         i++;
     } while (ocrValue > 65535); // While ocrValue is too large, try larger prescaler.
 
-    TCNT1 = 0; // Set counter to 0.
     TCCR1B |= i; // Set prescaler.
     set_bit(TCCR1B, WGM12); // Set CTC mode.
 
@@ -43,6 +62,18 @@ void timer1_freq_Hz(unsigned char hz, unsigned char ocr) {
     }
 }
 
-void timer1_freq_ms(unsigned char ms) {
-	// TODO
+/*
+ * Enable timer1 at desired time interval.
+ *
+ * @param unsigned char ms
+ *      Time interval of interrupt in ms
+ *
+ * @param unsigned char ocr
+ *      Output compare register to use
+ *
+ * @return void
+*/
+void timer1_freq_ms(uint16_t ms, unsigned char ocr) {
+    uint32_t hz = ms * 1000; // f = 1 / (ms / 1000)
+    timer1_freq_Hz(hz, ocr);
 }
