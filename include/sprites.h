fffe245d83ae6d3cd32606304bcbd7219b0cc52c
Shaun Karran <shaun.karran@gmail.com>
2014-11-07 16:30:44 +1100

    Added sprite files and created a tank sprite.

---
+++ include/sprites.h
@@ -0,0 +1,14 @@
+// Information ----------------------------------------------------------------
+
+/*  Library:        libTeensy20
+ *  Purpose:        ASCII font tables
+ *  Author(s):      Shaun Karran
+ *  Created:        October 2014
+*/
+
+#ifndef _SPRITES_H
+#define _SPRITES_H
+
+extern const unsigned char sprite_tank[];
+
+#endif //_SPRITES_H
