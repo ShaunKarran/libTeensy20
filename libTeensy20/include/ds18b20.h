6963ffec54c2e0ee02705154e38155f1c401aeb0
Shaun Karran <shaun.karran@gmail.com>
2015-07-14 22:10:00 +1000

    Added skeleton files for ds18b20 temperature sensor drivers.

---
+++ libTeensy20/include/ds18b20.h
@@ -0,0 +1,20 @@
+// Information ----------------------------------------------------------------
+
+/*	Library:		libTeensy20
+ *	Purpose:		Drivers to interface with the DS18B20 tempurature sensor
+ *	Author(s):		Shaun Karran
+ *	Created:		October 2014
+*/
+
+#ifndef _ds18b20_h
+#define _ds18b20_h
+
+// Defines --------------------------------------------------------------------
+
+
+
+// Function Prototypes --------------------------------------------------------
+
+
+
+#endif // _ds18b20_h
---
+++ libTeensy20/src/ds18b20.c
@@ -0,0 +1,20 @@
+// Information ----------------------------------------------------------------
+
+/*	Library:		libTeensy20
+ *	Purpose:		Drivers to interface with the DS18B20 tempurature sensor
+ *	Author(s):		Shaun Karran
+ *	Created:		October 2014
+*/
+
+// Includes -------------------------------------------------------------------
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdint.h>
+
+#include "../include/ds18b20.h"
+#include "../include/bitwise.h"
+
+// Function Definitions -------------------------------------------------------
+
+
