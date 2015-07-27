698ba8bd7eceb29a93bc7871ae4c5a9881f16d34
Shaun Karran <shaun.karran@gmail.com>
2015-07-27 23:26:09 +1000

    Added test program for the temp sensor.

---
+++ libTeensy20/src/ds18b20_test.c
@@ -0,0 +1,55 @@
+/*	Library:		libTeensy20
+ *	Purpose:		Test for DS18B20
+ *	Author(s):		Shaun Karran
+ *	Created:		October 2014
+*/
+
+#include <avr/io.h>
+#include <stdint.h>
+
+#include "../include/bitwise.h"
+#include "../include/ds18b20.h"
+#include "../include/global_defines.h"
+#include "../include/lcd_5110.h"
+
+int main(void) {
+	uint16_t tempurature = 0;
+
+	cpu_speed(CPU_8Mhz);
+	lcd_init(LCD_DEFAULT_CONTRAST);
+
+	set_bit(DDRD, PD6);
+
+	while(1)
+	{
+		if(ds18_reset())
+		{
+			ds18_write_byte(0xCC);
+			ds18_write_byte(0x44);
+			while (ds18_read_byte() == 0) { ; }
+			if (ds18_reset())
+			{
+				ds18_write_byte(0xCC);
+				ds18_write_byte(0xBE);
+				tempurature = ds18_read_byte();
+				tempurature = ds18_read_byte() << 8;
+				lcd_print_int(tempurature);
+				if (tempurature < 21)
+				{
+					set_bit(PORTB, PB6);
+				} else
+				{
+					clr_bit(PORTB, PB6);
+				}
+			} else
+			{
+				lcd_print_str("Reset 2 Failed");
+			}
+		} else
+		{
+			lcd_print_str("Reset Failed");
+		}
+	}
+
+	return 0;
+}
--- libTeensy20/test/test.c
+++ libTeensy20/test/test.c
@@ -4,57 +4,14 @@
  *	Created:		October 2014
 */
 
-#include <avr/interrupt.h>
+#include <avr/io.h>
 
-#include "../include/adc.h"
-#include "../assets/ASCII_lib.h"
-#include "../include/bitwise.h"
-#include "../include/lcd_5110.h"
-#include "../include/global_defines.h"
-#include "../include/gfx.h"
-#include "../assets/sprites.h"
-#include "../include/timer.h"
-
-volatile uint16_t TICK = 0;
-
-int main(void) {
-
-	cpu_speed(CPU_8Mhz);
-
-	//timer Test --------------------------------------------------------------
-	timer1_freq_ms(20, TIMER_OCR_A);
-
-	//lcd_5110 Test -----------------------------------------------------------
-	lcd_init(LCD_DEFAULT_CONTRAST);
-
-	// lcd_goto(0, 0);
-	// lcd_print_str("Test String123");
-
-	// lcd_goto(0, 5);
-	// lcd_print_int(2123456789L);
-
-	// lcd_gfx Test -----------------------------------------------------------
-	gfx_init(LCD_X, LCD_Y);
-
-	while(1)
-	{
-		gfx_clear_buffer();
-
-		gfx_draw_sprite(sprite_smile, 30, 20);
-
-		gfx_draw_sprite(sprite_tank, TICK, 10);
-
-		gfx_draw_char('T', ASCII, 10, 35);
-
-		gfx_draw_string("Test.", ASCII, 60, TICK);
-
-		lcd_display_buffer(gfxBuffer);
-	}
-
-	return 0;
-}
-
-ISR(TIMER1_COMPA_vect)
+int main()
 {
-	TICK++;
+	DDRD |= (1 << PD6);
+	PORTD |= (1 << PD6);
+
+	while (1) { ; }
+	
+	return 0;
 }
